7e85a428229370482c1c122e1cc935fc17304bb7
Your Name <you@example.com>
2017-07-06 21:34:23 -0700

    1.2

--- index.js
+++ index.js
@@ -2,24 +2,31 @@ var express = require('express')
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var xss = require('xss');
+
+var chatxss = new xss.FilterXSS({
+  whiteList: [],
+  stripIgnoreTag: true,
+  stripIgnoreTagBody: ['script']
+});
 
 var users = [];
 
 app.use(express.static('public'))
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.send('<h1>Hello world</h1>');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
   console.log('new connection');
 
   var addedUser = false;
 
-  socket.on('send message', function(content, channel){
+  socket.on('send message', function(content, channel) {
     if (addedUser) {
       console.log(`message sent from ${socket.username} in ${channel}: ${content}`);
-      io.sockets.emit('message', content, channel, socket.username);
+      io.sockets.emit('message', chatxss.process(content), channel, socket.username);
     }
   });
 
@@ -27,10 +34,10 @@ io.on('connection', function(socket){
     if (addedUser) return;
     if (users.indexOf(username) > -1) return;
 
-    socket.username = username;
+    socket.username = chatxss.process(username);
     addedUser = true;
 
-    users.push(username)
+    users.push(socket.username)
 
     socket.emit('login', {
       users: users
@@ -41,11 +48,11 @@ io.on('connection', function(socket){
     io.sockets.emit('message', `${socket.username} joined.`, 'general', 'dorpchat');
   });
 
-  socket.on('disconnect', function () {
+  socket.on('disconnect', function() {
     if (addedUser) {
       console.log('user disconnected')
       var i = users.indexOf(socket.username);
-      if(i != -1) {
+      if (i != -1) {
         users.splice(i, 1);
       }
       io.sockets.emit('update userlist', users);
@@ -54,6 +61,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(8080, function(){
+http.listen(8080, function() {
   console.log('listening on *:8080');
 });
--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,39 @@ function MainViewModel() {
   this.username = ko.observable();
   this.channel = ko.observable('general');
   this.users = ko.observableArray();
-  this.messageContent = ko.observable();
+  this.messageContent = ko.observable('');
+
+  this.messagewriterKeypress = function(data, event) {
+    var keyCode = (event.which ? event.which : event.keyCode);
+    if (keyCode == 13) {
+      this.sendMessage();
+      return false;
+    }
+    return true;
+  }
+
+  this.sendNotification = function(title, body) {
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    }
+
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+      // If it's okay let's create a notification
+      var notification = new Notification(title, {body: body});
+    }
+
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== "denied") {
+      Notification.requestPermission(function(permission) {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          var notification = new Notification("You will now recive notifications from DorpChat");
+        }
+      });
+    }
+  }
 
   this.setUsername = function() {
     socket.emit('set username', this.username())
@@ -21,8 +53,15 @@ function MainViewModel() {
     }
   }
 
-  socket.on('message', function(content, message, sender) {
-    self.messages.push({content:content, message:message, sender:sender});
+  socket.on('message', function(content, channel, sender) {
+    if (document.hidden) {
+      self.sendNotification(sender, content);
+    }
+    self.messages.push({
+      content: micromarkdown.parse(content),
+      channel: channel,
+      sender: sender
+    });
   })
 
   socket.on('login', function(data) {
@@ -30,7 +69,6 @@ function MainViewModel() {
   })
 
   socket.on('update userlist', function(users) {
-    console.log('u')
     self.users(users);
   })
 }
